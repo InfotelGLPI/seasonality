d9c5c032565496bd4af02463f5868161bf82ad1c
Xavier CAILLAUD <tsmr@users.noreply.github.com>
2015-07-21 16:12:36 +0200

    Update loadscripts.php

--- ajax/loadscripts.php
+++ ajax/loadscripts.php
@@ -1,28 +1,24 @@
 <?php
 
 /*
-  -------------------------------------------------------------------------
-  Metademands plugin for GLPI
-  Copyright (C) 2013 by the Metademands Development Team.
-  -------------------------------------------------------------------------
-
-  LICENSE
-
-  This file is part of Metademands.
-
-  Metademands is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  Metademands is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with Metademands. If not, see <http://www.gnu.org/licenses/>.
-  --------------------------------------------------------------------------
+ -------------------------------------------------------------------------
+ Seasonality plugin for GLPI
+ Copyright (C) 2003-2015 by the Seasonality Development Team.
+ https://forge.indepnet.net/projects/seasonality
+ -------------------------------------------------------------------------
+ LICENSE
+ This file is part of Seasonality.
+ Seasonality is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ Seasonality is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with Seasonality. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
  */
 
 include ('../../../inc/includes.php');
