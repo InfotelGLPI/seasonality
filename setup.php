2ec7da9db37c01a4b0a901a6e0d97895e1f24cad
keha <kevin.hainry@infotel.com>
2018-05-29 09:19:55 +0200

    Update seasonality for 9.2

--- setup.php
+++ setup.php
@@ -48,15 +48,17 @@ function plugin_init_seasonality() {
       // Add specific files to add to the header : javascript or css
       $PLUGIN_HOOKS['add_javascript']['seasonality'] = ["lib/daterangepicker/jquery.comiseo.daterangepicker.min.js",
                                                              "lib/daterangepicker/moment.min.js"];
-      
+
       if (strpos($_SERVER['REQUEST_URI'], "ticket.form.php") !== false
             || strpos($_SERVER['REQUEST_URI'], "helpdesk.public.php") !== false
             || strpos($_SERVER['REQUEST_URI'], "tracking.injector.php") !== false) {
-         $PLUGIN_HOOKS['add_javascript']['seasonality'][] = 'scripts/seasonality.js';
-         $PLUGIN_HOOKS['add_javascript']['seasonality'][] = 'scripts/seasonality_load_scripts.js';
+         $PLUGIN_HOOKS['javascript']['seasonality'][] = '/plugins/seasonality/scripts/seasonality.js';
+         $PLUGIN_HOOKS['javascript']['seasonality'][] = '/plugins/seasonality/scripts/seasonality_load_scripts.js';
+
+         Html::requireJs('seasonality');
       }
-      
-      // Css 
+
+      // Css
       $PLUGIN_HOOKS['add_css']['seasonality'] = ["lib/daterangepicker/jquery.comiseo.daterangepicker.css"];
 
       // Purge
